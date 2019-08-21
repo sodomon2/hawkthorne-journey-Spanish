e82bf73a70eb7589d1ad51355649808217573997
Victor Diego Alegandro Diaz Urbaneja <sodomon2@gmail.com>
2019-08-21 00:35:49 -0400

    Traduciendo chickenfinger.lua

--- items/consumables/chickenfinger.lua
+++ items/consumables/chickenfinger.lua
@@ -1,9 +1,9 @@
 -- made by Nicko21
 return{
   name = "chickenfinger",
-  description = "Chicken Finger",
+  description = "Dedo de pollo",
   type = "consumable",
-  info = '"To victory. It feels unfamiliar but it tastes like chicken."',
+  info = '"Por la victoria. No me resulta familiar, pero sabe a pollo."',
   MAX_ITEMS = 1,
   width = 24,
   consumable = {
