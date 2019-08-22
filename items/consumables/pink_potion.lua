a9f39205840a24c7081489375fb681f0bbe08a70
Victor Diego Alegandro Diaz Urbaneja <sodomon2@gmail.com>
2019-08-22 02:25:13 -0400

    Traduciendo pink_potion.lua

--- items/consumables/pink_potion.lua
+++ items/consumables/pink_potion.lua
@@ -1,9 +1,9 @@
 -- made by Nicko21
 return{
   name = "pink_potion",
-  description = "Max Health Potion",
+  description = "Max Pocion de Salud",
   type = "consumable",
-  info = 'a full heal potion',
+  info = 'una pocion de curacion completa',
   MAX_ITEMS = 2,
   width = 24,
   consumable = {heal = 'max'},
