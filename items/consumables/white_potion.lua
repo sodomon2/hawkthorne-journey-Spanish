321e36c3cfb2e2d207ae3073de40ac4a5aeff2de
Victor Diego Alegandro Diaz Urbaneja <sodomon2@gmail.com>
2019-08-22 02:26:36 -0400

    Traduciendo white_potion.lua

--- items/consumables/white_potion.lua
+++ items/consumables/white_potion.lua
@@ -1,9 +1,9 @@
 -- made by Nicko21
 return{
   name = "white_potion",
-  description = "Greater Health Potion",
+  description = "Pocion para la salud",
   type = "consumable",
-  info = 'heals for 50hp',
+  info = 'sana para 50hp',
   MAX_ITEMS = 2,
   width = 24,
   consumable = {heal = 50},
