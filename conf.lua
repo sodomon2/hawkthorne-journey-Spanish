7ca9e6241823ccae4183018e72ca6eee15172680
Victor Diego Alegandro Diaz Urbaneja <sodomon2@gmail.com>
2020-04-04 00:09:22 -0400

    Corrigiendo conf.lua

--- conf.lua
+++ conf.lua
@@ -3,7 +3,7 @@ function love.conf(t)
     t.url               = "http://projecthawkthorne.com"
     t.author            = "https://github.com/hawkthorne?tab=members"
     t.version           = "0.9.2"
-    t.identity          = "hawkthorne_release"
+    t.identity          = "hawkthorne"
     t.window.width      = 1056
     t.window.height     = 672
     t.window.fullscreen = false
